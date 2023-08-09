5a9c1f272fefabaf988cc29fbaacf7f0a81c1213
Iamneo <techadmin@iamneo.ai>
2023-08-09 17:35:33 +0000

    Backup Commit

--- reactapp/src/components/UI/Card/Card.jsx
+++ reactapp/src/components/UI/Card/Card.jsx
@@ -3,8 +3,10 @@ import Button from "../Button/Button";
 const Card = ({ question, correctAnswerMarkUpdate, attempt, options, answer, setQsns, qsn, setCard, qsnAttempt }) => {
     // Creating an array of options
     const optionsArray = [options.option1, options.option2, options.option3, options.option4];
-    // State variable to track the marked status of the buttonconst [marked, setMarked] = useState(false);
-    // Event handler to handle the click event on the optionsconst click = (clicked) => {
+    // State variable to track the marked status of the button
+    const [marked, setMarked] = useState(false);
+    // Event handler to handle the click event on the options
+    const click = (clicked) => {
     // Increase the question attempted
     setQsns(qsn + 1)
     // Mark the button as clicked
@@ -16,18 +18,19 @@ const Card = ({ question, correctAnswerMarkUpdate, attempt, options, answer, set
     if (clicked === answer)
         // Increase the score if the answer is correct
         correctAnswerMarkUpdate(attempt + 1)
-}
-// Styles for the button
-const norm = "bg-red-300 px-2 py-1 rounded hover:bg-orange-400"
-const disable = "bg-orange-300 px-2 py-1 rounded disabled:opacity-50"
-return (
-    <div>
-        <h4 className="pt-3 pb-1">{question}</h4>
-        <div className="flex space-x-2">
-            {optionsArray.map((option, index) =>
-                <Button key={index} marked={marked} onClick={() => click(option)} style={!marked ? norm : disable} >{option}</Button>
-            )}
+    }
+    // Styles for the button
+    const norm = "bg-red-300 px-2 py-1 rounded hover:bg-orange-400";
+    const disable = "bg-orange-300 px-2 py-1 rounded disabled:opacity-50";
+    return (
+        <div>
+            <h4 className="pt-3 pb-1">{question}</h4>
+            <div className="flex space-x-2">
+                {optionsArray.map((option, index) =>
+                    <Button key={index} marked={marked} onClick={() => click(option)} style={!marked ? norm : disable} >{option}</Button>
+                )}
+            </div>
         </div>
-    </div>
-)
+    )
+}
 export default Card;
